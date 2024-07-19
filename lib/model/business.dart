780daa0ea62d1aeccf92796b223782ef187e0607
Luis <heredialuis20@gmail.com>
2024-07-19 20:44:47 +0200

    first commit

---
+++ lib/model/business.dart
@@ -0,0 +1,27 @@
+class Business {
+  final String id;
+  final String cif;
+  final String name;
+  final String phone;
+  final String address;
+  final String email;
+  final String createdAt;
+
+  Business(
+      {required this.id,
+        required this.cif,
+        required this.name,
+        required this.phone,
+        required this.address,
+        required this.email,
+        required this.createdAt});
+
+  Business.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        cif = json['cif'],
+        name = json['name'],
+        phone = json['phone'],
+        address = json['address'],
+        email = json['email'],
+        createdAt = json['createdAt'];
+}
