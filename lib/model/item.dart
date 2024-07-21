ba85a7ca541a3b722e154fa83d3d59ede7680d67
Luis <heredialuis20@gmail.com>
2024-07-21 09:40:50 +0200

    Added ItemStatus in item class

--- lib/model/item.dart
+++ lib/model/item.dart
@@ -1,22 +1,36 @@
+enum ItemStatus{
+  active,
+  inactive,
+  suspended,
+  noStock,
+  unhandledError
+}
+
 class Item {
   final String id;
   final String name;
   final String description;
   final String imageUrl;
+  final ItemStatus status;
 
   Item({
     required this.id,
     required this.name,
     required this.description,
     required this.imageUrl,
+    required this.status,
   });
 
+
+  //TODO: SE DEBE MANEJAR LA ENTREGA DEL STATUS
+  //DE STRING A ItemStatus
   factory Item.fromJson(Map<String, dynamic> json) {
     return Item(
       id: json['id'],
       name: json['title'] as String,
       description: json['description'] as String,
       imageUrl: json['imageUrl'] as String,
+      status: json['status']
     );
   }
 
@@ -25,6 +39,7 @@ class Item {
       'title': name,
       'description': description,
       'imageUrl': imageUrl,
+      'status': status
     };
   }
 }
