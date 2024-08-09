364e4e5b556c171c02c8d7b0526ca5119a69562d
Luis <heredialuis20@gmail.com>
2024-08-09 06:06:32 +0200

    Implemented: new refresh token management with the secure storage

--- lib/core/network/jwt_token_manager.dart
+++ lib/core/network/jwt_token_manager.dart
@@ -3,6 +3,8 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 class JwtTokenManager {
   final _storage = const FlutterSecureStorage();
 
+  /// ***************** JWT TOKEN ******************
+
   Future<void> saveToken(String token) async {
     await _storage.write(key: 'jwt_token', value: token);
   }
@@ -13,5 +15,19 @@ class JwtTokenManager {
 
   Future<void> deleteToken() async {
     await _storage.delete(key: 'jwt_token');
+  }
+
+  /// *************** REFRESH TOKEN ****************
+
+  Future<void> saveRefreshToken(String refreshToken) async{
+    await _storage.write(key: 'jwt_refreshToken', value: refreshToken);
+  }
+
+  Future<String?> getRefreshToken() async{
+    return await _storage.read(key: 'jwt_refreshToken');
+  }
+
+  Future<void> deleteRefreshToken() async{
+    await _storage.delete(key: 'jwt_refreshToken');
   }
 }
