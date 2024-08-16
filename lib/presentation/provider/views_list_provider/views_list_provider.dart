7ae7cba4628ce30e5b254ec88b4f6c0f4e7b4872
Luis <heredialuis20@gmail.com>
2024-08-16 19:28:13 +0200

    Implemented: first implementation to a qr_scanner_view

--- lib/presentation/provider/views_list_provider/views_list_provider.dart
+++ lib/presentation/provider/views_list_provider/views_list_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import '../../views/forms/login_user_view.dart';
+import '../../views/qr_scanner_view.dart';
 import '../../views/settings_view.dart';
 import '../../views/shifts_view.dart';
 
@@ -7,10 +8,13 @@ class ViewsListProvider extends ChangeNotifier {
   Widget _shiftView = const ShiftsView();
   Widget _profileView = LoginUserView();
   Widget _settingsView = const SettingsView();
+  Widget _qrScannerView = QrScannerView();
 
   Widget get getShiftView => _shiftView;
 
   Widget get getProfileView => _profileView;
+
+  Widget get getQrScannerView => _qrScannerView;
 
   Widget get getSettingsView => _settingsView;
 
@@ -21,6 +25,11 @@ class ViewsListProvider extends ChangeNotifier {
 
   set setProfileView(Widget profileView) {
     _profileView = profileView;
+    notifyListeners();
+  }
+
+  set setQrScannerView(Widget qrScannerView) {
+    _qrScannerView = qrScannerView;
     notifyListeners();
   }
 
