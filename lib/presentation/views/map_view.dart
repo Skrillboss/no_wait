f7321a3b47c4a7eab3bb08b7f86aed551337523a
Luis <heredialuis20@gmail.com>
2024-08-18 10:31:36 +0200

    Implemented: new method to obtain the location permission

--- lib/presentation/views/map_view.dart
+++ lib/presentation/views/map_view.dart
@@ -2,17 +2,20 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class MapView extends StatefulWidget{
+class MapView extends StatefulWidget {
   const MapView({super.key});
 
   @override
   State<MapView> createState() => MapViewState();
 }
 
-class MapViewState extends State<MapView>{
+class MapViewState extends State<MapView> {
   final Completer<GoogleMapController> _controller =
-  Completer<GoogleMapController>();
+      Completer<GoogleMapController>();
+
+  bool _hasLocationPermission = false;
 
   Future<LatLng> getDeviceLocation() async {
     LatLng location = const LatLng(37.43296265331129, -122.08832357078792);
@@ -21,7 +24,8 @@ class MapViewState extends State<MapView>{
       distanceFilter: 100,
     );
 
-    Position position = await Geolocator.getCurrentPosition(locationSettings: locationSettings);
+    Position position =
+        await Geolocator.getCurrentPosition(locationSettings: locationSettings);
 
     location = LatLng(position.latitude, position.longitude);
 
@@ -33,7 +37,7 @@ class MapViewState extends State<MapView>{
     zoom: 14.4746,
   );
 
-  CameraPosition getCamaraPosition(LatLng currentLocation){
+  CameraPosition getCamaraPosition(LatLng currentLocation) {
     return CameraPosition(
         bearing: 192.8334901395799,
         target: currentLocation,
@@ -47,21 +51,48 @@ class MapViewState extends State<MapView>{
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
 
+  @override
+  void initState() {
+    super.initState();
+    _checkLocationPermission();
+  }
 
+  Future<void> _checkLocationPermission() async {
+    PermissionStatus status = await Permission.location.request();
+
+    setState(() {
+      _hasLocationPermission = status == PermissionStatus.granted;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        myLocationEnabled: true,
-        myLocationButtonEnabled: true,
-        zoomControlsEnabled: false,
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
+      body: _hasLocationPermission
+          ? GoogleMap(
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+              zoomControlsEnabled: false,
+              mapType: MapType.hybrid,
+              initialCameraPosition: _kGooglePlex,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+            )
+          : Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.green, Colors.blue],
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft),
+              ),
+              child: const Center(
+                child: Text(
+                  'Necesito los permisos de la cámara para continuar.',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+            ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: _goToMyLocation,
         label: const Text('Go to my Location!'),
@@ -73,6 +104,7 @@ class MapViewState extends State<MapView>{
   Future<void> _goToMyLocation() async {
     final LatLng deviceLocation = await getDeviceLocation();
     final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(getCamaraPosition(deviceLocation)));
+    await controller.animateCamera(
+        CameraUpdate.newCameraPosition(getCamaraPosition(deviceLocation)));
   }
 }
