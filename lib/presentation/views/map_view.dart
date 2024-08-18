f902b904de86eb91e68caaa537ef5ea265f0d3cc
Luis <heredialuis20@gmail.com>
2024-08-18 06:48:18 +0200

    Stable version without qr_code_scanner

--- lib/presentation/views/map_view.dart
+++ lib/presentation/views/map_view.dart
@@ -1,10 +1,11 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapView extends StatefulWidget{
   const MapView({super.key});
-  
+
   @override
   State<MapView> createState() => MapViewState();
 }
@@ -13,10 +14,32 @@ class MapViewState extends State<MapView>{
   final Completer<GoogleMapController> _controller =
   Completer<GoogleMapController>();
 
+  Future<LatLng> getDeviceLocation() async {
+    LatLng location = const LatLng(37.43296265331129, -122.08832357078792);
+    const LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 100,
+    );
+
+    Position position = await Geolocator.getCurrentPosition(locationSettings: locationSettings);
+
+    location = LatLng(position.latitude, position.longitude);
+
+    return location;
+  }
+
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(40.41831, -3.70275),
     zoom: 14.4746,
   );
+
+  CameraPosition getCamaraPosition(LatLng currentLocation){
+    return CameraPosition(
+        bearing: 192.8334901395799,
+        target: currentLocation,
+        tilt: 59.440717697143555,
+        zoom: 19.151926040649414);
+  }
 
   static const CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
@@ -24,12 +47,14 @@ class MapViewState extends State<MapView>{
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
 
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
         myLocationEnabled: true,
-        myLocationButtonEnabled: false,
+        myLocationButtonEnabled: true,
         zoomControlsEnabled: false,
         mapType: MapType.hybrid,
         initialCameraPosition: _kGooglePlex,
@@ -38,15 +63,16 @@ class MapViewState extends State<MapView>{
         },
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
+        onPressed: _goToMyLocation,
+        label: const Text('Go to my Location!'),
+        icon: const Icon(Icons.location_on),
       ),
     );
   }
 
-  Future<void> _goToTheLake() async {
+  Future<void> _goToMyLocation() async {
+    final LatLng deviceLocation = await getDeviceLocation();
     final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    await controller.animateCamera(CameraUpdate.newCameraPosition(getCamaraPosition(deviceLocation)));
   }
 }
--- lib/presentation/views/qr_scanner_view.dart
+++ lib/presentation/views/qr_scanner_view.dart
@@ -1,7 +1,4 @@
-import 'dart:io';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class QrScannerView extends StatefulWidget {
   const QrScannerView({super.key});
@@ -10,62 +7,19 @@ class QrScannerView extends StatefulWidget {
   State<QrScannerView> createState() => _QrScannerViewState();
 }
 
-class _QrScannerViewState extends State<QrScannerView>{
-
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-  Barcode? result;
-  QRViewController? controller;
-
-  // In order to get hot reload to work we need to pause the camera if the platform
-  // is android, or resume the camera if the platform is iOS.
-  @override
-  void reassemble() {
-    super.reassemble();
-    if (Platform.isAndroid) {
-      controller!.pauseCamera();
-    } else if (Platform.isIOS) {
-      controller!.resumeCamera();
-    }
-  }
-
+class _QrScannerViewState extends State<QrScannerView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 5,
-            child: QRView(
-              key: qrKey,
-              onQRViewCreated: _onQRViewCreated,
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Center(
-              child: (result != null)
-                  ? Text(
-                  'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}')
-                  : const Text('Scan a code'),
-            ),
-          )
-        ],
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Colors.green, Colors.blue],
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft),
+        ),
       ),
     );
-  }
-
-  void _onQRViewCreated(QRViewController controller) {
-    this.controller = controller;
-    controller.scannedDataStream.listen((scanData) {
-      setState(() {
-        result = scanData;
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    controller?.dispose();
-    super.dispose();
   }
 }
