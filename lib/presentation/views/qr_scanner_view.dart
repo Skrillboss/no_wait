221c2691a0bbab706db9ff196835f0471a803dc4
Luis <heredialuis20@gmail.com>
2024-08-18 08:51:17 +0200

    Implemented: Widget build if permission is denied

--- lib/presentation/views/qr_scanner_view.dart
+++ lib/presentation/views/qr_scanner_view.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
@@ -12,28 +13,90 @@ class QrScannerView extends StatefulWidget {
 class _QrScannerViewState extends State<QrScannerView>
     with WidgetsBindingObserver {
   final MobileScannerController _mobileScannerController =
-      MobileScannerController(
-        detectionSpeed: DetectionSpeed.noDuplicates
-      );
+  MobileScannerController(detectionSpeed: DetectionSpeed.noDuplicates);
 
   StreamSubscription<Object?>? _subscription;
+
+  bool _hasCameraPermission = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // Start listening to lifecycle changes.
+    WidgetsBinding.instance.addObserver(this);
+
+    // Check camera permission and start the scanner if permission is granted.
+    _checkCameraPermission();
+  }
+
+  Future<void> _checkCameraPermission() async {
+    PermissionStatus status = await Permission.camera.request();
+
+    setState(() {
+      _hasCameraPermission = status == PermissionStatus.granted;
+    });
+
+    if (_hasCameraPermission) {
+      // Start listening to the barcode events.
+      _subscription = _mobileScannerController.barcodes.listen(_handleBarcode);
+
+      // Start the scanner itself.
+      unawaited(_mobileScannerController.start());
+    }
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    // If the controller is not ready or permission is not granted, do not try to start or stop it.
+    if (!_hasCameraPermission || !_mobileScannerController.autoStart) {
+      return;
+    }
+
+    switch (state) {
+      case AppLifecycleState.detached:
+      case AppLifecycleState.hidden:
+      case AppLifecycleState.paused:
+        unawaited(_mobileScannerController.stop());
+        unawaited(_subscription?.cancel());
+        _subscription = null;
+        break;
+      case AppLifecycleState.resumed:
+      // Restart the scanner when the app is resumed.
+      // Don't forget to resume listening to the barcode events.
+        _subscription =
+            _mobileScannerController.barcodes.listen(_handleBarcode);
+        unawaited(_mobileScannerController.start());
+        break;
+      case AppLifecycleState.inactive:
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: MobileScanner(
-          controller: _mobileScannerController,
-      onDetect: (capture) {
-        final List<Barcode> barcodes = capture.barcodes;
-        for(final barcode in barcodes){
-          print('The information of this barcode is: ${barcode.rawValue}');
-        }
-      },
-    ));
+      body: _hasCameraPermission
+          ? MobileScanner(
+          controller: _mobileScannerController, onDetect: _handleBarcode)
+          : Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Colors.green, Colors.blue],
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft),
+        ),
+        child: const Center(
+          child: Text(
+            'Necesito los permisos de la cámara para continuar.',
+            style: TextStyle(color: Colors.white, fontSize: 18),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
-  Future<void> dispose() async {
+  void dispose() {
     // Stop listening to lifecycle changes.
     WidgetsBinding.instance.removeObserver(this);
     // Stop listening to the barcode events.
@@ -42,5 +105,13 @@ class _QrScannerViewState extends State<QrScannerView>
     // Dispose the widget itself.
     super.dispose();
     // Finally, dispose of the controller.
+    _mobileScannerController.dispose();
+  }
+
+  void _handleBarcode(BarcodeCapture barCodeCapture) {
+    final List<Barcode> barcodes = barCodeCapture.barcodes;
+    for (final barcode in barcodes) {
+      print('The information of this barcode is: ${barcode.rawValue}');
+    }
   }
 }
